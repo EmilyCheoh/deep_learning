00989d1c1ef180870e2bd6c73072db2b1734b7df
Emily Qiao <xiaoliq@uw.edu>
2020-12-09 02:24:44 -0600

    Updates

---
+++ precache-manifest.9dfa54a098f6b167cfd23567543a8843.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fa777793f443a0881cc9cd1c6a08791",
+    "url": "/deep_learning/index.html"
+  },
+  {
+    "revision": "7f5f809340f24dc224a9",
+    "url": "/deep_learning/static/css/2.3e9a6110.chunk.css"
+  },
+  {
+    "revision": "2ccf1dd00a96b0c5ad21",
+    "url": "/deep_learning/static/css/main.2078d671.chunk.css"
+  },
+  {
+    "revision": "7f5f809340f24dc224a9",
+    "url": "/deep_learning/static/js/2.979818d1.chunk.js"
+  },
+  {
+    "revision": "22c538655c10a98aa9bc46d79d90c54d",
+    "url": "/deep_learning/static/js/2.979818d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ccf1dd00a96b0c5ad21",
+    "url": "/deep_learning/static/js/main.dba9585d.chunk.js"
+  },
+  {
+    "revision": "b1214560070f7ab20019",
+    "url": "/deep_learning/static/js/runtime-main.7ffa3657.js"
+  }
+]);
